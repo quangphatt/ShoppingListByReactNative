0e6babd03f48ec92c27dae32533d49986c36f189
Quang Phat <phatquang2811@gmail.com>
2022-01-22 08:52:03 +0700

    add README, list item, addItem, removeItem

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import uuid from 'react-native-uuid';
+
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
-  return (
-    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
-    </View>
-  );
+    const [items, setItems]=useState([
+        {id: uuid.v4(), text: 'Milk'},
+        {id: uuid.v4(), text: 'Egg'},
+        {id: uuid.v4(), text: 'Bread'},
+        {id: uuid.v4(), text: 'Juice'},
+    ]);
+    
+    const deleteItem=(id)=>{
+        setItems((prevItems)=>prevItems.filter((item) => item.id !== id));
+    };
+
+    const addItem=(txt)=>{
+        if(!txt){
+            Alert.alert(
+                'No item entered',
+                'Please enter an item when adding to your shopping list',
+                [{
+                    text: 'OK',
+                    style: 'cancel',
+                },
+                ],
+                {cancelable: true},
+            )
+        }
+        else {
+            setItems(prevItems=>[{id: uuid.v4(), text: txt}, ...prevItems]);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Header />
+            <AddItem addItem={addItem}/>
+            <FlatList 
+                data={items}
+                renderItem={({item})=>(
+                    <ListItem 
+                        item={item}
+                        deleteItem={deleteItem}
+                    />
+                )}
+            />
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        // paddingTop: 60   // iOS
+    },
+});
 
 export default App;
 
---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple Shopping List app to practice React Native
+Reference: https://www.youtube.com/watch?v=Hf4MJH0jDb4
